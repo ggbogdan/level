5f914f82c1217148041f5512ba068f6fac0bcbb3
ggbogdan <93436950+ggbogdan@users.noreply.github.com>
2022-12-18 23:54:37 +0200

    Update test-pipeline.groovy
    update

--- test-pipeline.groovy
+++ test-pipeline.groovy
@@ -17,13 +17,12 @@ node {
 
     stage('Complete any setup steps') {
         echo "Complete set-up steps"
-        echo "${octoperf_test_value}"
+        echo "${test_value}"
     }
 
     stage('Execute Performance Tests') {
         dir("${WORKSPACE}") {
             sh "/usr/jmeter/bin/jmeter.sh -n -t $SCRIPT_PATH -Jusers=$USERS -JrampUp=$RAMP_UP -Jloop=$LOOP"
-//             bat "c:/apache-jmeter/apache-jmeter/bin/jmeter.bat -n -t Shift-Left.jmx -l Shift-Left.jtl -Joptestvalue=${octoperf_test_value}"
         }
     }
 
