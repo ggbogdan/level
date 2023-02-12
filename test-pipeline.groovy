101f09b16026d8327d1c84b94c69e88185184c12
ggbogdan <93436950+ggbogdan@users.noreply.github.com>
2023-02-12 22:32:53 +0200

    Update test-pipeline.groovy

--- test-pipeline.groovy
+++ test-pipeline.groovy
@@ -56,9 +56,6 @@ pipeline {
                from: "fortestjenkins@ukr.net",
             subject: "jenkins build:${currentBuild.currentResult}: ${env.JOB_NAME}",
                body: "${currentBuild.currentResult}: Job ${env.JOB_NAME}\nMore Info can be found here: ${env.BUILD_URL}"
-        }
-    }
-}
              }
         }
 }
