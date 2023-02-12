c36153ada69ac5d874fbe6b578ab6bbf9cd40eae
ggbogdan <93436950+ggbogdan@users.noreply.github.com>
2023-02-12 21:42:22 +0200

    Update test-pipeline.groovy

--- test-pipeline.groovy
+++ test-pipeline.groovy
@@ -27,16 +27,16 @@ node {
 
         }
     }  
-    stage('Send e-mail') {
-    post
-        {
-        always{
-            mail to: "fortestjenkins@ukr.net",
-            subject: "Test Email",
-            body: "Test"
-               }
-         }
-    }
+//     stage('Send e-mail') {
+//     post
+//         {
+//         always{
+//             mail to: "fortestjenkins@ukr.net",
+//             subject: "Test Email",
+//             body: "Test"
+//                }
+//          }
+//     }
     step([$class: 'ArtifactArchiver', artifacts: 'test.jtl'])
     
 
@@ -58,3 +58,38 @@ node {
 //         }
 //     }
 // }
+def notifyBuild(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus = buildStatus ?: 'SUCCESS'
+
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def summary = "${subject} (${env.BUILD_URL})"
+  def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'SUCCESS') {
+    color = 'GREEN'
+    colorCode = '#00FF00'
+  } else {
+    color = 'RED'
+    colorCode = '#FF0000'
+  }
+
+  // Send notifications
+  slackSend (color: colorCode, message: summary)
+
+  hipchatSend (color: color, notify: true, message: summary)
+
+  emailext (
+      subject: subject,
+      body: details,
+      recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+    )
+}
