339f848b3d07053a343b6707521baf15bad74231
ggbogdan <93436950+ggbogdan@users.noreply.github.com>
2023-02-16 18:27:55 +0200

    Update test-pipeline.groovy

--- test-pipeline.groovy
+++ test-pipeline.groovy
@@ -2,7 +2,7 @@
 node {
 
     stage('Initialise') {
-        /* Checkout the scripts */
+
         checkout scm: [
                 $class: 'GitSCM',
                 userRemoteConfigs: [
@@ -15,13 +15,11 @@ node {
         ], poll: false
     }
 
-    stage('Complete any setup steps') {
-        echo "Complete set-up steps"
-        echo "${test_value}"
-    }
 
-    stage('Execute Performance Tests') {
-        dir("${WORKSPACE}") {
+    stage('Execute Performance Test') 
+    {
+        dir("${WORKSPACE}") 
+        {
             sh "/usr/jmeter/bin/jmeter.sh -n -t $SCRIPT_PATH -Jusers=$USERS -JrampUp=$RAMP_UP -Jloop=$LOOP -l test.jtl -e -o report -f"
             sh "sudo cp -r /var/lib/jenkins/workspace/test2/report/ /opt/tomcat/webapps/"
 
@@ -29,25 +27,27 @@ node {
     }  
 
     step([$class: 'ArtifactArchiver', artifacts: 'test.jtl'])
-    
 
 }
-pipeline {
+
+pipeline 
+{
     agent any
     stages {
-        stage('Hello') {
-            steps {
-                echo "Hello world"
+        stage('Sending e-mail') 
+                {
+            steps  {
+                echo "Sending e-mail"
                     }
-                      }
-          }
+                 }
+            }
     post {
         always {
             mail to: "fortestjenkins@ukr.net",
                from: "fortestjenkins@ukr.net",
             subject: "jenkins build:${currentBuild.currentResult}: ${env.JOB_NAME}",
                body: "${currentBuild.currentResult}: Job ${env.JOB_NAME}\nMore Info can be found here: ${env.BUILD_URL} and here: http://192.168.56.102:8080/report/"
-             }
+              }
         }
 }
 
